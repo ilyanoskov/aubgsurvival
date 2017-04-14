9bfd0153ae7afccad4965f8c03cebeb510f389cc
ilyanoskov <riesig88@gmail.com>
2017-04-14 21:57:10 +0300

    submodule changes2

--- client/src/actions/kill.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export function kill(code) {
-    return dispatch => {
-        return axios.post('http://localhost:3001/api/kill', code);
-    };
-}
--- client/src/actions/signupActions.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-
-export function userSignupRequest(userData) {
-    return dispatch => {
-        return axios.post('http://localhost:3001/api/users/register', userData);
-    }
-}
