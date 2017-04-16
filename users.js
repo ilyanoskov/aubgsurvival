76f6343536117f5c4f3d70e128fea2d20db211e6
ilyanoskov <riesig88@gmail.com>
2017-04-16 16:14:48 +0300

    adding aubg email validation

--- users.js
+++ users.js
@@ -60,9 +60,9 @@ const validateInput = async (req) => {
         errors.email = 'Email is invalid';
     }
     //Check if it's an AUBG e-mail
-if (!data.email.substring(6,13) === "@aubg.edu") {
+    if (!data.email.substring(6) === "@aubg.edu") {
     errors.email = 'Must be an aubg email'
-}
+    }
     
     //Check if password was supplied
     if (Validator.isEmpty(data.password
