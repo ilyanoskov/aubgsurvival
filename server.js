0b78c2be3c73b829bed1eb8dd325fe7cbe6c2e73
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-10 16:19:19 +0200

    Updates

--- server.js
+++ server.js
@@ -14,63 +14,75 @@ const stats = require('./statistics').stats;
 const victims = require('./victims');
 
 const mongoose = require('mongoose');
-const db = mongoose.connect(process.env.MONGODB_URI);
+const db = mongoose.connect(process.env.MONGODB_URI, {
+  promiseLibrary: Promise
+}).then(()=>{
+  console.log(`--------------------`)
+  console.log(`connected to the database`)
+});
+
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 
 //error wrapper, courtesy to Spanish guys
 app.use((error, request, response, next) => {
-    response.status(400).send('Oops there was an error, ask administrator');
-    console.log(error);
-    next();
-})
+  response.status(400).send('Oops there was an error, ask administrator');
+  console.log(error);
+  next();
+});
 
 app.post('/api', (req, res) => {
-    console.log(req.body);
-    res.send('OK');
+  console.log(req.body);
+  res.send('OK');
 });
 
 let whitelist = [
-  'http://localhost:3000',      //this is my front-end url for development
-   'https://ilyanoskov.github.io',
-   'http://aubgsurvival.fun',
-   'http://www.aubgsurvival.fun'
+  'http://localhost:3000', //this is my front-end url for development
+  'https://ilyanoskov.github.io',
+  'http://aubgsurvival.fun',
+  'http://www.aubgsurvival.fun'
 ];
 
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+  origin: function(origin, callback) {
+    if (true) {
+      //if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
   },
-    credentials : true
-}
+  credentials: true
+};
 
 app.use(cors(corsOptions));
 
 app.get('/api/users', users.users);
 app.post('/api/users/register', users.register);
-app.delete('/api/users', users.delete);
+app.delete('/api/users', users.delete); //requires secret
 app.delete('/api/user', users.deleteUser); //requires secret
-app.get('/api/users/personal',  authenticate, users.personal);
+app.get('/api/users/personal', authenticate, users.personal);
 
 //Auth
-app.post('/api/auth',auth.auth);
+app.post('/api/auth', auth.auth);
 
 //events api
-app.get('/api/events',events.get);
+app.get('/api/events', events.get);
 app.delete('/api/events', events.erase); //requires secret
 
 //gameplay
 app.post('/api/assign', victims.initialAssign); //requires secret
-app.post('/api/kill',authenticate, kill.kill);
+app.post('/api/kill', authenticate, kill.kill);
 app.get('/api/stats', stats);
 
-app.set('port', (process.env.PORT || 3001));
+app.set('port', process.env.PORT || 3001);
 
-app.listen(app.get('port'), console.log('Listening on the port ', app.get('port')));
+app.listen(
+  app.get('port'),
+  console.log('Listening on the port ', app.get('port'))
+);
